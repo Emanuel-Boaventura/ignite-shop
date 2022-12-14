1fb51461c93fec448756cbde9933e4c1ebf88c38
Emanuel-Boaventura <emanuelbmatos321@gmail.com>
2022-12-14 11:35:09 -0400

    syle: add carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/image';
+import { useKeenSlider } from 'keen-slider/react';
+
 import { HomeContainer, Product } from '../styles/pages/home';
 
 import camiseta1 from '../assets/camisetas/1.png';
 import camiseta2 from '../assets/camisetas/2.png';
-// import camiseta3 from '../assets/camisetas/3.png';
+import camiseta3 from '../assets/camisetas/3.png';
+
+import 'keen-slider/keen-slider.min.css';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <figure>
           <Image src={camiseta1} width={520} height={480} alt='' />
 
@@ -20,9 +31,33 @@ export default function Home() {
         </figure>
       </Product>
 
-      <Product>
+      <Product className='keen-slider__slide'>
         <figure>
           <Image src={camiseta2} width={520} height={480} alt='' />
+
+          <figcaption>
+            <strong>Camiseta X</strong>
+
+            <span>R$ 79,90</span>
+          </figcaption>
+        </figure>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <figure>
+          <Image src={camiseta3} width={520} height={480} alt='' />
+
+          <figcaption>
+            <strong>Camiseta X</strong>
+
+            <span>R$ 79,90</span>
+          </figcaption>
+        </figure>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <figure>
+          <Image src={camiseta3} width={520} height={480} alt='' />
 
           <figcaption>
             <strong>Camiseta X</strong>
