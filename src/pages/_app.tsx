fd6767f55ee35abbc2f8cfb646c2b224f8fb0957
Emanuel-Boaventura <emanuelbmatos321@gmail.com>
2022-12-16 10:05:32 -0400

    style: add cartButton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 import type { AppProps } from 'next/app';
 import Image from 'next/image';
+import { Handbag } from 'phosphor-react';
 import { globalStyles } from '../styles/global';
 import logoImg from '../assets/logo.svg';
-import { Container, Header } from '../styles/pages/app';
+import { Container, Header, CartButton } from '../styles/pages/app';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const itemQuantity = 1;
+
   return (
     <Container>
       <Header>
         <Image src={logoImg} alt='' />
+
+        <CartButton hasItem={itemQuantity > 0}>
+          <Handbag size={24} weight='bold' />
+
+          <span>{itemQuantity}</span>
+        </CartButton>
       </Header>
 
       <Component {...pageProps} />
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -10,8 +10,52 @@ export const Container = styled('div', {
 })
 
 export const Header = styled('header', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
   padding: '2rem 0',
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+})
+
+export const CartButton = styled('button', {
+  padding: '0.75rem',
+  backgroundColor: '$gray800',
+  color: '$gray300',
+  borderRadius: 6,
+  border: 'none',
+  cursor: 'pointer',
+  position: 'relative',
+  lineHeight: 0,
+
+  span: {
+    display: 'none'
+  },
+
+  variants: {
+    hasItem: {
+      true: {
+        span: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '1.625rem',
+          height: '1.625rem',
+          borderRadius: "100%",
+          position: 'absolute',
+          backgroundColor: '$green500',
+          color: '$gray300',
+          border: '3px solid #121214',
+          top: 0,
+          right: 0,
+          transform: 'translate(25%,-25%)'
+        }
+      }
+    }
+  },
+
+  '&:hover': {
+    color: '$gray100',
+  }
 })
